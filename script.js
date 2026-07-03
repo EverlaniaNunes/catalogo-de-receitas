70a0ab646a8aef987514c029c0a996a4cb24fbc4
everlanianunes <emnn@discente.ifpe.edu.br>
2026-07-03 13:06:22 -0300

    receitas

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
 fetch("https://dummyjson.com/recipes")
   .then((resposta) => resposta.json())
   .then((dados) => {
-    console.log(dados);
+
+    const sectionReceitas = document.getElementById("receitas");
+
+    dados.recipes.forEach((receita) => {
+
+      sectionReceitas.innerHTML += `
+        <article class="bg-white rounded-lg shadow-md overflow-hidden flex flex-col">
+
+          <img
+            src="${receita.image}"
+            alt="${receita.name}"
+            class="w-full h-48 object-cover"
+          >
+
+          <div class="p-4 flex flex-col gap-2">
+
+            <h2 class="text-xl font-bold">
+              ${receita.name}
+            </h2>
+
+            <p>
+              ⏱️ Tempo: ${receita.prepTimeMinutes} minutos
+            </p>
+
+            <p>
+              ⭐ Dificuldade: ${receita.difficulty}
+            </p>
+
+          </div>
+
+        </article>
+      `;
+
+    });
+
   });
